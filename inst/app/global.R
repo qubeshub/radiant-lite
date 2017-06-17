49f864dc8a89b0ff3c5768d2f1a5bc10f00f4cff
mdlama <mdlama@wm.edu>
2017-06-17 16:54:56 -0400

    Modifications after running devtools::check()

--- inst/app/global.R
+++ inst/app/global.R
@@ -9,14 +9,14 @@ source(file.path(getOption("serenity.path.data"), "app/global.R"), encoding = ge
 ## Keeping the rest around for possible future use
 
 ## setting path for figures in help files
-# addResourcePath("figures_design", file.path(getOption("radiant.path.design"),"app/tools/help/figures/"))
+# addResourcePath("figures_design", file.path(getOption("serenity.path.design"),"app/tools/help/figures/"))
 
 ## setting path for www resources
-# addResourcePath("www_design", file.path(getOption("radiant.path.design"),"app/www/"))
+# addResourcePath("www_design", file.path(getOption("serenity.path.design"),"app/www/"))
 
 ## loading url information
-# source(file.path(getOption("radiant.path.design"), "app/init.R"), encoding = getOption("radiant.encoding"), local = TRUE)
+# source(file.path(getOption("serenity.path.design"), "app/init.R"), encoding = getOption("serenity.encoding"), local = TRUE)
 
 ##
 
-options(radiant.url.patterns = make_url_patterns())
+options(serenity.url.patterns = make_url_patterns())
